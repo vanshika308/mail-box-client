234285a93f3689035b6380e2835fa0d77dbdc32d
vanshika308 <vanshikatiwari0308@gmail.com>
2024-01-17 22:07:04 +0530

    Making the mails Realtime

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,29 +1,25 @@
 import classes from './Welcome.module.css';
-
 import React, { Fragment, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { authAction } from '../storeRedux/authReducer';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { mailSliceAction } from '../storeRedux/emailReducer';
-
 const HomePage = () => {
   const dispatch=useDispatch();
   const [reRender,setreRender]=useState(true);
   const unRead=useSelector(state=>state.mail.unRead)
   const myEmail=localStorage.getItem('email').replace(/['@','.']/g,'');
-
   let intervalID;
   intervalID = setInterval(()=>{
     setreRender((prev)=>!prev);
     console.log('intervall',intervalID);
-  }, 7000);
+  }, 3000);
 
   const clearInteravl=()=>{
     clearInterval(intervalID);
     console.log(intervalID);
   }
-
   const logoutHandler =()=>{
     dispatch(authAction.logout());
   }
@@ -33,7 +29,7 @@ const HomePage = () => {
         const reponse=await fetch(`https://book-search-app-62511-default-rtdb.firebaseio.com/inbox/${myEmail}.json`);
 
         const mailData=await reponse.json();
-        // console.log('useEffectcalled', mailData);
+        console.log('useEffectcalled');
         for(let key in mailData){
             // data=[{id:key,...mailData[key]},...data]
             if(mailData[key].dot===true){
@@ -42,16 +38,15 @@ const HomePage = () => {
             }
         }
         // console.log(noOfUnread,'noOfUnread');
-
         dispatch(mailSliceAction.updateUnread(noOfUnread))
 
     }
     fetchDaata();
+    clearInterval(intervalID);
 },[reRender])
 
   return (
     <Fragment>
-      {/* <button onClick={clearInteravl}>clearInteravl</button> */}
       <div className={classes.main}>
         <div className={classes.header}>
           <div className={classes.welcome}>Welcome to Mail Box</div>
